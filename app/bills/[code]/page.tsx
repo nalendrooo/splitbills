4f045983537b5b89a8917378fa269fcd8d973f27
Nalendrooo <nalendroadil274@gmail.com>
2024-08-27 01:00:37 +0700

    all test

--- app/bills/[code]/page.tsx
+++ app/bills/[code]/page.tsx
@@ -3,19 +3,20 @@ import axios from 'axios'
 import React from 'react'
 const page = async ({ params }: any) => {
     // const params = useParams()
-    // const data = await GetBill(params.code)
+    const data = await GetBill(params?.code)
 
     // console.log(params.code)
     // console.log(params)
-    // console.log(data?.data?.data)
+    console.log(data?.data?.data)
     return (
         <div>{params.code}</div>
     )
 }
 
 function GetBill(code?: string) {
+    if (!code) return
 
-    return axios.get('http://localhost:3000/api/bills/' + code)
+    return axios.get('http://localhost:3000/api/bills/5MSDVM')
 }
 
 export default page
